c9619e29260ff66997130037a8f2d9b9dc4b68f4
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-27 10:29:43 +0530

    mastr

---
+++ src/main/java/com/nestdigital/nestcompany/NestcompanyApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.nestcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestcompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestcompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/nestcompany/NestcompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.nestcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestcompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
