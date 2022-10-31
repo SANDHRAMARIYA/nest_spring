ca03ea5d7d247ecdcd764d1dd72eb609a784cc4a
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-31 21:17:23 +0530

    api

--- src/main/java/com/nestdigital/nestcompany/Dao/LogDao.java
+++ src/main/java/com/nestdigital/nestcompany/Dao/LogDao.java
@@ -19,6 +19,11 @@ public interface LogDao extends CrudRepository<LogModel,Integer> {
 
     @Query(value="SELECT l.`id`, l.`empcode`, l.`login_time`, l.`login_security_code`, l.`logout_time`, l.`logout_security_code`,e.name as emp_name,s1.name as login_sec_name, s2.name as logout_sec_name FROM `logs` l JOIN employee e JOIN security s1 JOIN security s2 ON l.empcode = e.empcode AND l.login_security_code = s1.security_code AND l.logout_security_code = s2.security_code WHERE l.empcode = :empcode",nativeQuery = true)
     public List<Map<String,String>> viewLogByEmpId(Integer empcode);
+
+
+    @Query(value ="SELECT * FROM `logs` WHERE `logout_security_code`=0",nativeQuery = true)
+    public List<LogModel> viewCheckIn();
 }
+
 
 
